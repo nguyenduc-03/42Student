432241facf9f97c21f93d56a6e3519e3ae3779ab
Nguyen Huy Thai Duc <Thai.NguyenHuy@Student.HTW-Berlin.de>
2024-11-29 00:10:01 +0100

    chatgpt get_new_line

--- get_new_line/get_next_line.h
+++ get_new_line/get_next_line.h
@@ -1,14 +1,15 @@
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024 // Default buffer size if not defined
-# endif
+#define BUFFER_SIZE 99999
 
-# include <stdlib.h>
-# include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-// Function prototype
-char *get_next_line(int fd);
+char    *get_next_line(int fd);
+size_t  gnl_strlen(const char *str);
+char    *gnl_strdup(const char *src, size_t len);
+char    *gnl_strjoin(char *s1, char *s2);
+int     gnl_has_newline(const char *str);
 
 #endif
