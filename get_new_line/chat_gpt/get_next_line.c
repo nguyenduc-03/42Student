687a17f45d588f5041051caa62dd6146340c225f
Nguyen Duc <thaiduc0706@gmail.com>
2024-12-09 12:13:37 +0000

    can handle big buffer but still timeout

--- get_new_line/chat_gpt/get_next_line.c
+++ get_new_line/chat_gpt/get_next_line.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/09 12:02:26 by codespace         #+#    #+#             */
-/*   Updated: 2024/12/09 12:04:58 by codespace        ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   get_next_line.c									:+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: codespace <codespace@student.42.fr>		+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2024/12/09 12:02:26 by codespace		 #+#	#+#			 */
+/*   Updated: 2024/12/09 12:10:38 by codespace		###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include "get_next_line.h"
@@ -40,19 +40,23 @@ static char	*get_line(char **saved)
 	return (line);
 }
 
-static ssize_t	read_file(int fd, char **saved)
+static	ssize_t	read_file(int fd, char **saved)
 {
-	char	buffer[BUFFER_SIZE + 1];
+	char	*buffer;
 	ssize_t	bytes_read;
 	char	*temp;
 
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (-1);
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read > 0)
+	if (bytes_read >= 0)
 	{
 		buffer[bytes_read] = '\0';
 		temp = ft_strjoin(*saved, buffer);
 		*saved = temp;
 	}
+	free(buffer);
 	return (bytes_read);
 }
 
