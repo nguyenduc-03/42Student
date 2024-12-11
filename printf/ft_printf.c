84d0fcf3b5be4eca22585771d49b592235bf415f
Duc Nguyen <ducnguye@c3a4c5.42berlin.de>
2024-12-11 13:31:01 +0100

    finish gnl

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -1,79 +1,56 @@
-#include <stdio.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ducnguye <ducnguye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 15:16:50 by ducnguye          #+#    #+#             */
+/*   Updated: 2024/11/27 19:18:54 by ducnguye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
 #include <stdarg.h>
 
-/*
-• %c Prints a single character.
-• %s Prints a string (as defined by the common C convention).
-• %p The void * pointer argument has to be printed in hexadecimal format.
-• %d Prints a decimal (base 10) number.
-• %i Prints an integer in base 10.
-• %u Prints an unsigned decimal (base 10) number.
-• %x Prints a number in hexadecimal (base 16) lowercase format.
-• %X Prints a number in hexadecimal (base 16) uppercase format.
-• %% Prints a percent sign.
-*/
-
-
-char *find_symbol(const char *str) //return the array of string whenever hit a %, move it to a new index
+/* int	ft_atoi_base(char *str, char *base);
+void	ft_string( char *str, ...) // print the string of char
 {
-    int i = 0;
-    int j = 0;
-    while(*str)
-    {
-        if(*str == '%')
-        { 
-            //check for the next letter
-            if(*(str + 1) == c ||*(str + 1) == s) 
-            {
-                           
-            }
-        }
-        
-        j++;
-        str++;
-    }
-    return array;
+
 }
+void	ft_number( char *str, ...); */
 
-int	ft_putstr(const char *s)
+int	ft_printf(const char *str, ...)
 {
-    int i = 0;
-	if (!s)
-		return -1;
-	while (*s)
+	va_list	agruments;
+	va_start(agruments, str);
+	char	option;
+	int		i;
+
+	i = 0;
+	while (str[i])
 	{
-		write(1, s, 1);
-		s++;
-        i++;
+		if (str[i] == '%')
+		{
+			option = str[i + 1];
+			if (option == 's')
+			{
+				ft_putstr_fd (va_arg(agruments, char *), 1);
+				i = i + 2;
+			}
+		}
+		else
+		{
+			write (1, &str[i], 1);
+			i++;
+		}
 	}
-    return (i);
+	va_end(agruments);
+	return (i);
 }
-
-int ft_p(const char *str, ...)
-{
-    va_list agruments;
-
-    //a function to go though str and check for %d, and return back   "Hello %s baby %d\n", "David"
-    //find_symbol(str);
-
-
-    va_start(agruments, str);
-    int return_value = 0;
-
-    const char *s = va_arg(agruments, const char*);
-    //Search though the string, to find %c,%d,%s...
-    if(s)
-        return_value = ft_putstr(s);
-    va_end(agruments);
-    return return_value;
-    
-}
-
 
 int main()
 {
-    ft_p("Hi my lady","Hi I undetstand now\n");
-    printf("asd%dsda");
-    return 0;
+	ft_printf("try: %s%s\n", "hello");
+	return 0;
 }
