2bea3294b6c1907a074bc7ee4a0a90e0a5fee9d5
Nguyen Huy Thai Duc <Thai.NguyenHuy@Student.HTW-Berlin.de>
2025-01-26 18:14:46 +0100

    can not compile

---
+++ push_swap/inc/push_swap.h
@@ -0,0 +1,15 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+
+int		is_integer(const char *str);
+int		ft_atoi(const char *str);
+int		error(void);
+int		ft_isdigit(int c);
+
+#endif
---
+++ push_swap/src/ft_atoi.c
@@ -0,0 +1,30 @@
+
+#include <unistd.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+	{
+		i++;
+	}
+	if (str[i] == '-')
+	{
+		sign = -sign;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
